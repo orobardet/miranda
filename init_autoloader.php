19bc8b0a7ba2d4f63df232aa642a9d410cf9cc1d
Olivier Robardet <olivier.robardet@server.fake>
2013-08-21 23:54:47 +0200

    Intégration ZF2 dans le dépôt
    - Mise à jour en même temps vers la v2.2.3 du ZF2.
    - Intégration directement du framework complet dans vendor, pour le commiter.
    - Retrait de composer et cie.
    - Simplifcation du l'init_autoloader.php.

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,49 +1,27 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-/**
- * This autoloading setup is really more complicated than it needs to be for most
- * applications. The added complexity is simply to reduce the time it takes for
- * new developers to be productive with a fresh skeleton. It allows autoloading
- * to be correctly configured, regardless of the installation method and keeps
- * the use of composer completely optional. This setup should work fine for
- * most users, however, feel free to configure autoloading however you'd like.
- */
-
-// Composer autoloading
-if (file_exists('vendor/autoload.php')) {
-    $loader = include 'vendor/autoload.php';
-}
-
 $zf2Path = false;
 
+// ZFZ peut-être dans le répertoire par défaut local, ou dans un path fourni dans la variable d'environnement ZF2_PATH
 if (is_dir('vendor/ZF2/library')) {
-    $zf2Path = 'vendor/ZF2/library';
-} elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
-    $zf2Path = getenv('ZF2_PATH');
-} elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
-    $zf2Path = get_cfg_var('zf2_path');
+	$zf2Path = 'vendor/ZF2/library';
+} elseif (getenv('ZF2_PATH')) {
+	$zf2Path = getenv('ZF2_PATH');
 }
 
 if ($zf2Path) {
-    if (isset($loader)) {
-        $loader->add('Zend', $zf2Path);
-    } else {
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        Zend\Loader\AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true
-            )
-        ));
-    }
+	// On tente d'abord de charger le ZF2 local ou défini par la variable d'environnement
+	include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+} else {
+	// Sinon, on tente de charger depuis l'include path PHP
+	include 'Zend/Loader/AutoloaderFactory.php';
 }
 
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+	throw new RuntimeException("Unable to load ZF2 (not found in default path 'vendor/ZF2/library'). You can define a 'ZF2_PATH' env variable.");
+} else {
+	Zend\Loader\AutoloaderFactory::factory(array(
+		'Zend\Loader\StandardAutoloader' => array(
+			'autoregister_zf' => true
+		)
+	));
 }
