271d5cc00d19671189f65f7fe2886ceba5cdb202
Olivier Robardet <orobardet@users.noreply.github.com>
2015-12-13 23:07:04 +0100

    MAJ ZendFramework 2.5.2

--- init_autoload.php
+++ init_autoload.php
@@ -1,13 +1,42 @@
 <?php
-require_once __DIR__ . '/vendor/zendframework/zendframework/library/Zend/Loader/AutoloaderFactory.php';
-require_once __DIR__ . '/vendor/zendframework/zendframework/library/Zend/Loader/ClassMapAutoloader.php';
+if (file_exists('vendor/autoload.php')) {
+    include 'vendor/autoload.php';
+}
+if (class_exists('Zend\Loader\AutoloaderFactory')) {
+    return;
+}
 
-Zend\Loader\AutoloaderFactory::factory(
-		array(
-			'Zend\Loader\ClassMapAutoloader' => array(
-				'Composer' => __DIR__ . '/vendor/composer/autoload_classmap.php'
-			),
-			'Zend\Loader\StandardAutoloader' => array(
-				'autoregister_zf' => true
-			)
-		));
+$zf2Path = false;
+if (getenv('ZF2_PATH')) {            // Support for ZF2_PATH environment variable
+    $zf2Path = getenv('ZF2_PATH');
+} elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
+    $zf2Path = get_cfg_var('zf2_path');
+}
+if ($zf2Path) {
+    if (isset($loader)) {
+        $loader->add('Zend', $zf2Path);
+        $loader->add('ZendXml', $zf2Path);
+    } else {
+        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+
+        $zf2AutoLoaderConfig = [];
+        if (file_exists(__DIR__ . '/vendor/composer/autoload_classmap.php')) {
+            $zf2AutoLoaderConfig += [
+                'Zend\Loader\ClassMapAutoloader' => [
+                    'Composer' => __DIR__ . '/vendor/composer/autoload_classmap.php'
+                ]
+            ];
+        }
+
+        $zf2AutoLoaderConfig += [
+            'Zend\Loader\StandardAutoloader' => [
+                'autoregister_zf' => true
+            ]
+        ];
+
+        Zend\Loader\AutoloaderFactory::factory($zf2AutoLoaderConfig);
+    }
+}
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2. Run composer or define a ZF2_PATH environment variable.');
+}
