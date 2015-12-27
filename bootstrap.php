92380ec03dc385a9c0d243d6f9a9d66cb786c250
Olivier Robardet <orobardet@users.noreply.github.com>
2015-12-27 23:04:33 +0100

    Fix affichage des erreurs en CLI
    Plus de HTML, elles sont affichées proprement.
    + deplacament du define REQUEST_MICROTIME en début du bootstrap plutôt que dans le index.html

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,18 @@
 <?php
+namespace Miranda;
 /**
  * Ce fichier contient le bootstrap de l'application Miranda.
  * Il doit être inclus dans tous les points d'entrée de l'application, que ce soit des scripts Web ou CLI 
  */
 // On défini le fuseau horaire par défaut de l'application
+define('REQUEST_MICROTIME', microtime(true));
 date_default_timezone_set('Europe/Paris');
 
+$g_bMirandaIsCli = (PHP_SAPI == 'cli');
+$g_bMirandaInDev = (getenv('APPLICATION_ENV') == 'dev');
+
 // Si on est en environnement de dev, affichage d'un maximum d'info en cas d'erreur
-if ((getenv('APPLICATION_ENV') == 'dev') || (PHP_SAPI == 'cli')) {
+if ($g_bMirandaInDev || $g_bMirandaIsCli) {
 	error_reporting(E_ALL | E_STRICT | E_NOTICE | E_DEPRECATED);
 	ini_set("display_errors", 1);
 	ini_set("display_startup_errors", 1);
@@ -17,7 +22,7 @@ if ((getenv('APPLICATION_ENV') == 'dev') || (PHP_SAPI == 'cli')) {
 function exception_error_handler($errno, $errstr, $errfile, $errline ) {
 	throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
 }
-set_error_handler("exception_error_handler");
+\set_error_handler('\Miranda\exception_error_handler');
 
 // On change le répertoire courant pour le parent, qui est la racine des sources du produit
 // Ainsi tous les chemins sont relatifs
@@ -28,18 +33,18 @@ require 'init_autoload.php';
 
 // Lancement de l'application
 try {
-	Zend\Mvc\Application::init(require 'config/application.config.php')->run();
-} catch (Exception $e) {
+	\Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+} catch (\Exception $e) {
 	while ($e) {
-		echo "<hr/>";
-		echo $e->getFile() . ":" . $e->getLine() . "<br/>";
-		echo "Exception code '" . $e->getCode() . "' with message: " . $e->getMessage() . "<br/>";
+		echo $g_bMirandaIsCli?"----------------------------".PHP_EOL:"<hr/>";
+		echo $e->getFile() . ":" . $e->getLine() . ($g_bMirandaIsCli?PHP_EOL:"<br/>");
+		echo "Exception code '" . $e->getCode() . "' with message: " . $e->getMessage() . ($g_bMirandaIsCli?PHP_EOL:"<br/>");
 		if (!$e->getPrevious()) {
 			$traces = $e->getTrace();
 			if (count($traces)) {
-				echo "<ul>";
+                if (!$g_bMirandaIsCli) echo "<ul>";
 				foreach ($traces as $trace) {
-					echo "<li>";
+                    echo $g_bMirandaIsCli?" - ":"<li>";
 					if (array_key_exists('file', $trace) && array_key_exists('line', $trace)) {
 						echo "{$trace['file']}:{$trace['line']} ";
 					}
@@ -49,9 +54,9 @@ try {
 					if (array_key_exists('function', $trace)) {
 						echo "{$trace['function']}";
 					}
-					echo "</li>";
+					echo $g_bMirandaIsCli?PHP_EOL:"</li>";
 				}
-				echo "</ul>";
+                if (!$g_bMirandaIsCli) echo "</ul>";
 			}
 		}
 		$e = $e->getPrevious();
